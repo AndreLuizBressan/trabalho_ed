84baa8e14dd575351f0d1cd275a2c06a149d6b34
André Luiz <andre@intexfy.com>
2024-12-14 18:05:32 -0300

    adiciona classe ListaInvertida

---
+++ lista_invertida.py
@@ -0,0 +1,18 @@
+class ListaInvertida:
+    def __init__(self):
+        self.resgistros = {}
+
+    def inserir(self, registro):
+        self.resgistros[registro.id] = registro
+
+    def procura_por_id(self, id):
+        return self.resgistros.get(id, None)
+
+    def deleta_por_id(self, id):
+        if id in self.resgistros:
+            del self.resgistros[id]
+
+    def mostra_todos(self):
+        for registro in self.resgistros.values():
+            print(vars(registro))
+            print()
